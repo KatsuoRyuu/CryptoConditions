fad2d61b0c7889ba8c7ef276663d347d1b1df97f
Anders Blenstrup-Pedersen <abp@ryuu.technology>
2019-01-28 03:16:56 +0000

    Update test/Types/Ed25519Sha256Test.php

--- test/Types/Ed25519Sha256Test.php
+++ test/Types/Ed25519Sha256Test.php
@@ -7,6 +7,11 @@
  */
 namespace KryuuCommon\CryptoConditionsTest\Types;
 
+
+namespace KryuuCommon\CryptoConditionsTest\Types
+
+use PHPUnit\Framework\TestCase;
+use KryuuCommon\CryptoConditions\Exception\TypeException;
 use KryuuCommon\CryptoConditions\Types\Ed25519Sha256;
 use PHPUnit\Framework\TestCase;
 
@@ -17,5 +22,25 @@ class Ed25519Sha256Test extends TestCase {
         
         $ed->setSignature('123456789012345678901234567899012');
     }
-    
+
+    /**
+     * @testdox                  When setting public key to a string, expected TypeException should be thrown
+     * @expectedException        TypeException
+     * @expectedExceptionMessage Public key must be a Buffer, was: string
+     */
+    public function testSetPublicKeyNotBufferException() {
+
+        (new Ed25519Sha256())->setPublicKey('Wrong key');
+    }
+
+    /**
+     * @testdox                  When setting public key to a integer, expected TypeException should be thrown
+     * @expectedException        TypeException
+     * @expectedExceptionMessage Public key must be a Buffer, was: integer
+     */
+    public function testSetPublicKeyNotBufferException() {
+
+        (new Ed25519Sha256())->setPublicKey(1);
+    }
+
 }
